d5c86bb0a253af4fca41e04053b7101c7947c0d3
Dewa Alyanida Farhan <dewsfarhan@gmail.com>
2025-09-10 17:07:38 +0700

    First commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create(['name' => 'SuperAdmin', 'email' => 'superadmin@gmail.com', 'role' => 'Superadmin', 'status' => 'Active', 'password' => 'superadmin']);
+        User::create(['name' => 'Administrator', 'email' => 'administrator@gmail.com', 'role' => 'Administrator', 'status' => 'Active', 'password' => 'administrator']);
+        User::create(['name' => 'Staff', 'email' => 'staff@gmail.com', 'role' => 'Staff', 'status' => 'Active', 'password' => 'staff']);
+    }
+}
